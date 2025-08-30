b65e1da6b740c76536c33bb6e266c75850b8cbce
Gabriel Gudiño <gmgudino.data@gmail.com>
2025-08-30 18:37:33 -0300

    Ejercicio 1 resuelto

--- src/tp2programacionestructurada/Ej1.java
+++ src/tp2programacionestructurada/Ej1.java
@@ -4,6 +4,8 @@
  */
 package tp2programacionestructurada;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gmgud_ita4nrw
@@ -14,7 +16,17 @@ public class Ej1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int anio;
+        
+        System.out.println("Ingrese un año: ");
+        anio = Integer.parseInt(input.nextLine());
+        
+        if ((anio % 4 == 0 && anio % 100 != 0)| (anio % 400 == 0) ){
+            System.out.println("El año " + anio + " es bisiesto.");
+        } else {
+            System.out.println("El año " + anio + " no es bisiesto.");
+        }
     }
     
 }
