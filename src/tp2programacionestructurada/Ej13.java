3b0004e2fb2018abfd32ac4797b9d08ee5c119df
Gabriel Gudiño <gmgudino.data@gmail.com>
2025-08-31 12:19:45 -0300

    Ejercicio 13 resuelto

---
+++ src/tp2programacionestructurada/Ej13.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2programacionestructurada;
+
+/**
+ *
+ * @author gmgud_ita4nrw
+ */
+public class Ej13 {
+        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double[] precios = {199.99, 299.5, 149.75, 399, 89.99};
+        
+        System.out.println("Precios originales: ");
+        funcionRecursiva(precios);
+        
+        precios[2] = 129.99;
+        
+        System.out.println("Precios modificados: ");
+        funcionRecursiva(precios);
+    }
+    static void funcionRecursiva(double[] lista){
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println("Precio: " + lista[i]);            
+        }
+    }
+}
